bc90214a3fb1b63d5f708d3a0a142cac19fde06b
Superskyyy <superskyyy@outlook.com>
2023-06-16 20:40:02 -0400

    Initialize project structure

---
+++ Makefile
@@ -0,0 +1,97 @@
+#  Copyright 2023 SkyAPM org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+VERSION ?= next
+
+# determine host platform
+ifeq ($(OS),Windows_NT)
+    OS := Windows
+else ifeq ($(shell uname -s),Darwin)
+    OS := Darwin
+else
+    OS := $(shell sh -c 'uname 2>/dev/null || echo Unknown')
+endif
+
+.PHONY: env
+env: poetry
+	poetry install --all-extras
+	poetry run pip install --upgrade pip
+
+.PHONY: poetry poetry-fallback
+# poetry installer may not work on macOS's default python
+# falls back to pipx installer
+poetry-fallback:
+	python3 -m pip install --user pipx
+	python3 -m pipx ensurepath
+	pipx install poetry
+	pipx upgrade poetry
+
+poetry:
+ifeq ($(OS),Windows)
+	-powershell (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
+	poetry self update
+else
+	-curl -sSL https://install.python-poetry.org | python3 -
+	poetry self update || $(MAKE) poetry-fallback
+endif
+
+.PHONY: install
+install: gen-basic
+	python3 -m pip install --upgrade pip
+	python3 -m pip install .[all]
+
+.PHONY: lint
+# flake8 configurations should go to the file setup.cfg E1101 must be checked in future (need upstream change of DrainBase)
+lint: clean
+	poetry run flake8 models/uri_drain
+	poetry run pylint --disable=all --enable=E0602,E0603 models deploy benchmarks tests
+
+.PHONY: fix
+# fix problems described in CodingStyle.md - verify outcome with extra care
+fix:
+	poetry run unify -r --in-place .
+	poetry run flynt -tc -v .
+
+.PHONY: license
+license: clean
+	docker run -it --rm -v $(shell pwd):/github/workspace ghcr.io/apache/skywalking-eyes/license-eye:20da317d1ad158e79e24355fdc28f53370e94c8a header check
+
+.PHONY: package
+package: clean gen
+	poetry build
+
+.PHONY: upload-test
+upload-test: package
+	poetry run twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+.PHONY: upload
+upload: package
+	poetry run twine upload dist/*
+
+.PHONY: clean
+# FIXME change to python based so we can run on windows
+clean:
+ifeq ($(OS),Windows)
+	if exist rd /s /q dist build
+	if exist rd /s /q r3*.tgz*
+	for /r %%G in (__pycache__) do @if exist "%%G" rd /s /q "%%G"
+	for /r %%G in (.pytest_cache) do @if exist "%%G" rd /s /q "%%G"
+	for /r %%G in (*.pyc) do @if exist "%%G" del /q "%%G"
+else
+	rm -rf dist build
+	rm -rf r3*.tgz*
+	find . -name "__pycache__" -exec rm -r {} +
+	find . -name ".pytest_cache" -exec rm -r {} +
+	find . -name "*.pyc" -exec rm -r {} +
+endif
---
+++ deploy/demo/demo_big_file_uri.py
@@ -0,0 +1,93 @@
+#  Copyright 2023 SkyAPM org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import json
+import logging
+import random
+import sys
+import time
+from os.path import dirname
+from models.uri_drain.template_miner import TemplateMiner
+from models.uri_drain.template_miner_config import TemplateMinerConfig
+import tracemalloc
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
+in_log_file = "Endpoint100_trivial.txt"
+in_log_file = "Endpoint100_trivial_3k_repeat.txt"
+in_log_file = "Endpoint100_trivial_500k_perf_bench.txt"
+in_log_file = 'Endpoint200_hard.txt'
+config = TemplateMinerConfig()
+config.load(dirname(__file__) + "/uri_drain.ini")
+config.profiling_enabled = True
+template_miner = TemplateMiner(config=config)
+
+
+def run():
+    line_count = 0
+
+    with open(in_log_file) as f:
+        lines = f.readlines()
+        print('shuffle')
+        random.seed(42)
+        random.shuffle(lines)
+
+    start_time = time.time()
+    batch_start_time = start_time
+    batch_size = 10000
+
+    for line in lines:
+        line = line.rstrip()
+        result = template_miner.add_log_message(line)
+        line_count += 1
+        if line_count % batch_size == 0:
+            time_took = time.time() - batch_start_time
+            rate = batch_size / time_took
+            logger.info(f"Processing line: {line_count}, rate {rate:.1f} lines/sec, "
+                        f"{len(template_miner.drain.clusters)} clusters so far.")
+            batch_start_time = time.time()
+        if result["change_type"] != "none":
+            result_json = json.dumps(result)
+            logger.info(f"Input ({line_count}): " + line)
+            logger.info("Result: " + result_json)
+
+
+# starting the memory monitoring
+tracemalloc.start()
+
+run()
+
+# displaying the memory
+print(tracemalloc.get_traced_memory())
+print(f'Memory used by the profiler: peak {tracemalloc.get_traced_memory()[1] / 10 ** 6} MB, '
+      f'current {tracemalloc.get_traced_memory()[0] / 10 ** 6}')
+
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')
+for stat in top_stats[:5]:
+    print(stat)
+
+tracemalloc.stop()
+
+sorted_clusters = sorted(template_miner.drain.clusters, key=lambda it: it.size, reverse=True)
+for cluster in sorted_clusters:
+    print(cluster)
+    extracted_regex = template_miner._get_template_parameter_extraction_regex(
+        cluster.get_template(), exact_matching=True)
+    print(f'regex extracted {extracted_regex}')
+print("Prefix Tree:")
+template_miner.drain.print_tree(max_clusters=1000)
+
+template_miner.profiler.report(0)
